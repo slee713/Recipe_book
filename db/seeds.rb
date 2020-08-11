8a7fcde64c10d11f1a3c698b8273ccb66b216549
slee713 <steven.lee1393@gmail.com>
2020-08-11 14:23:35 -0400

    fix repository issue

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Recipe.destroy_all
+Ingredient.destroy_all
+RecipeIngredient.destroy_all
+
+10.times do 
+    Recipe.create(name: Faker::Food.dish, duration_in_min: rand(1..100), difficulty: ["easy", "medium", "hard"].sample, vegetarian:[true, false].sample)
+end 
+
+
+30.times do 
+    Ingredient.create(name: Faker::Food.ingredient)
+end
+
+50.times do 
+    RecipeIngredient.create(recipe: Recipe.all.sample, ingredient: Ingredient.all.sample)
+end
