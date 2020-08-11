141977e6a0307ad86272c3d789ccde80b96b5d53
ClaFiore <claborghini@gmail.com>
2020-08-11 15:10:20 -0400

    CRUD on recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,18 @@
 class Recipe < ApplicationRecord
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+
+    validates :name, :difficulty, :duration_in_min, presence: true
+
+    def nice_veg
+        if self.vegetarian
+            return "Yes"
+        else
+            return "No"
+        end
+    end
+
+    def cap_difficulty
+        self.difficulty.titlecase
+    end
 end
